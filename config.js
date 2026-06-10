e3f2992346665c7973f289f7e4ca4d4b45dca458
stanhodina-oss <stanhodina@gmail.com>
2026-06-10 16:46:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.TIPOVACKA_CONFIG = {
   mode: "google",
 
   sheetUrl: "https://opensheet.elk.sh/1Ic_pOvVKLXVgtnrOKmJjbCdDy2JrlOUXW7qcuLZxykE",
-  scriptUrl: "https://script.google.com/u/0/home/projects/1KDKZA0IocCePvtWgO0dyNhEXIyk2QWD8-BvWrarkX-n-bMvbfj09gV4t/edit",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbx8luql10Er62iXHymDlR0ClSP5G_qJjDkUAy6YBOQ718-E8IRPmLvHqgeecHjOMRQ6/exec",
 
   refreshMs: 15000
 };
